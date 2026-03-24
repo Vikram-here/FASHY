9a3e005522805b1e6bf9cb17efda627f6878065d
vikram <vraj48219@gmail.com>
2026-03-24 14:26:33 +0530

    backend creating first page of app

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,17 @@
-const express=require('express');
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
 const app=express();
 const PORT=8080;
  
+// middleware
+app.use(express.json())
+app.use(cors())
+
+//api endpoints
+app.get('/',(req,res)=>{
+res.send("api work")
+})
 
 app.listen(PORT,()=>{
     console.log(`listening on port no ${PORT}`)
