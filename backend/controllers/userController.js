79d629348516c80a26d07403bcfb1a87d833f000
vikram <vraj48219@gmail.com>
2026-03-26 14:50:13 +0530

    make controller , model ,and routes

---
+++ backend/controllers/userController.js
@@ -0,0 +1,15 @@
+const loginUser=async (req,res)=>{
+
+}
+
+// route for user registration
+
+const registerUSer =async (req,res)=>{
+res.json({msg:"working"})
+}
+
+// route for admin
+const adminLogin=async (req,res)=>{
+
+}
+export {loginUser,registerUSer,adminLogin}
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,24 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/mongodb.js';
+import userRouter from './routes/userRoute.js';
 const app=express();
 const PORT=8080;
- 
+connectDB().then(()=>{
+    console.log("DB connected successfully")
+ }).catch((err)=>{
+    console.log("DB not connected")
+ })
 // middleware
 app.use(express.json())
 app.use(cors())
 
 //api endpoints
+
+app.use('/api/user',userRouter)
+
+
 app.get('/',(req,res)=>{
 res.send("api work")
 })
